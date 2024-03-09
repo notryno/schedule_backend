de85fc734bf5f8d04844931dd68445e9e0a81272
notryno <ryanclepcha@gmail.com>
2024-03-09 08:57:13 +0545

    Modified description in Task to be blank

--- task/models.py
+++ task/models.py
@@ -6,7 +6,7 @@ from authentication.models import CustomUser
 class Task(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateField(null=True, blank=True)
